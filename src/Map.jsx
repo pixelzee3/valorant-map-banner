424891207b4c2f176a3b5cbbfd719d6f42a76e03
pixelzee3 <bpixelzee@gmail.com>
2022-03-22 12:20:18 +0700

    Attempt to apply bg to Map component. It's still buggy..

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,5 +1,10 @@
-const Map = () => {
-  return <div className="h-32 w-full border-2 border-indigo-600 md:h-40"></div>;
+const Map = ({ mapName }) => {
+  const mapBGProperty = `${mapName ? `bg-${mapName}` : ''}`;
+  return (
+    <div
+      className={`h-32 md:h-40 w-full border-2 border-indigo-600 ${mapBGProperty} `}
+    ></div>
+  );
 };
 
 export default Map;
