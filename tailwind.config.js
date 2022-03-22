d21f4b498e093024cfc3fd2ce3528b8afbe203da
pixelzee3 <bpixelzee@gmail.com>
2022-03-22 07:17:36 +0700

    Add bg images to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'ascent': 'url("./src/images/loading_screen_ascent.webp")',
+        'bind': 'url("./src/images/loading_screen_bind.webp")',
+        'breeze': 'url("./src/images/loading_screen_breeze.webp")',
+        'haven': 'url("./src/images/loading_screen_haven.webp")',
+        'icebox': 'url("./src/images/loading_screen_icebox.webp")',
+        'split': 'url("./src/images/loading_screen_split.webp")',
+        'fracture': 'url("./src/images/loading_screen_fracture.webp")',
+      },
+    },
   },
   variants: {},
   plugins: [],
