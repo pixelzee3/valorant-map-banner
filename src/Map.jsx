730a6b333802d184dc09a859da5d6a04139f3118
pixelzee3 <bpixelzee@gmail.com>
2022-03-27 23:05:20 +0700

    stil combatting conditional background bug.

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,11 @@
-const Map = ({ mapName }) => {
+const Map = ({ mapName, imgURL }) => {
+  console.log(mapName);
+  console.log(imgURL);
+  
   return (
-    <div className={`h-32 w-full border-2 border-indigo-600 bg-${mapName} md:h-40`}></div>
+    <div
+      className={`h-32 w-full border-2 border-indigo-600 bg-[url('${imgURL}')] md:h-40`}
+    ></div>
   );
 };
 
--- src/Maps.jsx
+++ src/Maps.jsx
@@ -3,9 +3,7 @@ import Map from './Map';
 const Maps = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      <Map mapName="ascent" />
-      <Map mapName="bind" />
-      <Map mapName="haven" />
+      <Map mapName="ascent" imgURL="./images/loading_screen_ascent.webp" />
     </div>
     );
 };
