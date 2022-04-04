668e0cf1e4824dd117fce13c5194555ea5d1215a
pixelzee3 <bpixelzee@gmail.com>
2022-04-04 20:19:33 +0700

    Added filler map functionality.

--- src/Maps.jsx
+++ src/Maps.jsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
 import Map from './Map';
 
 const Maps = () => {
+  const [mapNames] = useState([
+    'ascent',
+    'bind',
+    'breeze',
+    'haven',
+    'icebox',
+    'split',
+    'fracture',
+  ]);
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <Map mapName="ascent" />
-      <Map mapName="bind" />
-      <Map mapName="breeze" />
-      <Map mapName="haven" />
-      <Map mapName="icebox" />
-      <Map mapName="split" />
-      <Map mapName="fracture" />
-      <Map />
+    <div className="group grid grid-cols-1 md:grid-cols-2">
+      {mapNames.map((mapName) => (
+        <Map key={mapName} mapName={mapName} />
+      ))}
+      {mapNames.length % 2 !== 0 ? <div className='h-0 md:h-auto overflow-hidden'><Map mapName="filler" /></div> : null}
     </div>
   );
 };
