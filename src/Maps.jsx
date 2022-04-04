4c5b8dab9c9a9a2f16adbd58fab485d0f2790356
pixelzee3 <bpixelzee@gmail.com>
2022-04-04 20:39:51 +0700

    Add customizable bg-pos functionality.

--- src/Maps.jsx
+++ src/Maps.jsx
@@ -2,22 +2,50 @@ import { useState } from 'react';
 import Map from './Map';
 
 const Maps = () => {
-  const [mapNames] = useState([
-    'ascent',
-    'bind',
-    'breeze',
-    'haven',
-    'icebox',
-    'split',
-    'fracture',
+  const [maps] = useState([
+    {
+      id: 0,
+      name: 'ascent',
+      bgPos: 'top',
+    },
+    {
+      id: 1,
+      name: 'bind',
+      bgPos: 'center',
+    },
+    {
+      id: 2,
+      name: 'breeze',
+      bgPos: 'center',
+    },
+    {
+      id: 3,
+      name: 'haven',
+      bgPos: 'center',
+    },
+    {
+      id: 4,
+      name: 'icebox',
+      bgPos: 'center',
+    },
+    {
+      id: 5,
+      name: 'split',
+      bgPos: 'center',
+    },
+    {
+      id: 6,
+      name: 'fracture',
+      bgPos: 'center',
+    },
   ]);
 
   return (
     <div className="group grid grid-cols-1 md:grid-cols-2">
-      {mapNames.map((mapName) => (
-        <Map key={mapName} mapName={mapName} />
+      {maps.map(map => (
+        <Map key={map.id} mapName={map.name} bgPos={map.bgPos} />
       ))}
-      {mapNames.length % 2 !== 0 ? <div className='h-0 md:h-auto overflow-hidden'><Map mapName="filler" /></div> : null}
+      {maps.length % 2 !== 0 ? <div className='h-0 md:h-auto overflow-hidden'><Map mapName="filler" /></div> : null}
     </div>
   );
 };
