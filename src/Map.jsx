d2976d89a45085767e76bceed69eba8086cd9e11
pixelzee3 <bpixelzee@gmail.com>
2022-04-05 11:58:32 +0700

    Handle click for map component.

--- src/Map.jsx
+++ src/Map.jsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 const Map = ({ mapName, bgPos }) => {
   const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
 
   return (
     <div
       className={`h-40 w-full bg-${mapName.toLowerCase()} ${
         bgPos ? 'bg-' + bgPos : 'bg-center'
       } relative bg-cover md:h-52`}
+      onClick={handleClick}
     >
       <div className={`w-full h-full ${clicked && mapName.toLowerCase() !== "filler" ? "bg-red-500 opacity-50" : ""}`}>
         <h2
-          className={`absolute bottom-2.5 left-2.5 font-sans text-4xl text-white md:text-5xl ${
+          className={`absolute select-none bottom-2.5 left-2.5 font-sans text-4xl text-white md:text-5xl ${
             mapName.toLowerCase() === 'filler' ? 'hidden' : ''
           }`}
         >
