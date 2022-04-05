eacd2ffb2c52a7bad4c233da6acc3b09d448114e
pixelzee3 <bpixelzee@gmail.com>
2022-04-05 07:22:00 +0700

    Adjust map sizes?

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,7 +1,7 @@
 const Map = ({ mapName, bgPos }) => {
   return (
     <div
-      className={`h-32 w-full bg-${mapName} ${bgPos ? "bg-" + bgPos : "bg-center"} bg-cover md:h-40`}
+      className={`h-40 w-full bg-${mapName} ${bgPos ? "bg-" + bgPos : "bg-center"} bg-cover md:h-52`}
     ></div>
   );
 };
